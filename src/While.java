2ac92d8f1d65e219271d71c80817ba4bd57272ff
natal <natalia---santos@hotmail.com>
2024-09-06 19:50:34 -0500

    While1_100

---
+++ src/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int inicio=0;
+
+        while (inicio<100){
+            System.out.println(++inicio);
+        }
+    }
+}
