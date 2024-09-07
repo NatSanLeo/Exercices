1d5291fa4ce4f8c3fb52548d1bab29bd21cdf6e0
natal <natalia---santos@hotmail.com>
2024-09-06 19:56:37 -0500

    WhileUserNumber

---
+++ src/ShowAllNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ShowAllNumbers {
+    public static void main(String[] args) {
+        int numeroUsuario;
+        int centinela = 0;
+        Scanner userNumber = new Scanner(System.in);
+        System.out.println("Ingrese el numero: ");
+        numeroUsuario = userNumber.nextInt();
+
+        while (centinela < numeroUsuario) {
+            System.out.print(centinela = centinela + 1);
+        }
+    }
+
+}
