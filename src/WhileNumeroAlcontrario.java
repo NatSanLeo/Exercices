d1a83f9771273d0c568b4abbee0ef4e988618d9e
natal <natalia---santos@hotmail.com>
2024-09-24 18:32:42 -0500

    While

---
+++ src/WhileNumeroAlcontrario.java
@@ -0,0 +1,14 @@
+
+public class WhileNumeroAlcontrario {
+
+    public static void main(String[] args) {
+        int num = 50;
+        int i = 0;
+        while ( i <= num) {
+            System.out.print(num - i + " ");
+            i++;
+        }
+
+    }
+
+}
