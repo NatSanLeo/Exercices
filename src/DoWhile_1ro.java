9e3fd4f553ab191f585b807f3224366cffd4b57f
natal <natalia---santos@hotmail.com>
2024-10-03 22:45:44 -0500

    While

---
+++ src/DoWhile_1ro.java
@@ -0,0 +1,12 @@
+public class DoWhile_1ro {
+
+    public static void main(String[] args) {
+        int contador = 1;
+        do {
+            System.out.println("Contador: " + contador);
+            contador++;
+        } while (contador <= 5);
+    }
+}
+
+
