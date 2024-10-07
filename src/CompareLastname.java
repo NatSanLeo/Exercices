12db1197b48efa572eff3c608df86c2531273e3a
natal <natalia---santos@hotmail.com>
2024-10-06 20:39:33 -0500

    Compare Strings

---
+++ src/CompareLastname.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompareLastname {
+    public static void main(String[] args) {
+        Scanner userData =new Scanner(System.in);
+
+        String lastNameOne="";
+        String lastNameTwo="";
+
+        System.out.print("Ingresa el primer apellido: " + lastNameOne);
+        lastNameOne=userData.nextLine();
+
+        System.out.print("Ingresa el segundo apellido: " + lastNameTwo);
+        lastNameTwo=userData.nextLine();
+
+        if (lastNameOne.equals(lastNameTwo)){
+            System.out.println("Los apellidos son igules.");
+
+        }else
+            System.out.println("Los apellidos son diferentes.");
+
+
+    }
+}
