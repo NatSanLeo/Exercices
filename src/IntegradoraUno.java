6e48900f4126817da13d06deffbc128baf86daf5
natal <natalia---santos@hotmail.com>
2024-12-16 14:33:21 -0500

    Vectores

---
+++ src/IntegradoraUno.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IntegradoraUno {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num;
+        System.out.println("Ingresa un número para saber si es par o impar:");
+        num = teclado.nextInt();
+        if (num == 0) {
+            System.out.println("El número ingresado es neutro aditivo");
+        } else if (num % 2 == 0) {
+            System.out.println("El número ingresado es par");
+        } else {
+            System.out.println("El número ingresado es impar");
+        }
+    }
+
+}
+
