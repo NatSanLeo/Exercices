ecf32ce393f1786b96c1316519207520e4dfdff9
natal <natalia---santos@hotmail.com>
2025-01-18 16:38:52 -0500

    While

---
+++ src/W_cantNumeros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class W_cantNumeros {
+
+    public static void main(String[] args) {
+        Scanner  numeroUsuario = new Scanner(System.in);
+        int numero;
+        int contadorCifras=0;
+
+        System.out.println("Ingrese un numero");
+        numero=numeroUsuario.nextInt();
+
+        while (numero>0 && numero <9){
+            System.out.println("una cifra");
+
+
+
+        }
+
+    }
+}
