26650de8b5687890379d8f20c1110025565d884e
natal <natalia---santos@hotmail.com>
2025-02-02 11:18:01 -0500

    fix while

--- src/Exercices_While/W_cantNumeros.java
+++ src/Exercices_While/W_cantNumeros.java
@@ -5,13 +5,19 @@ import java.util.Scanner;
 public class W_cantNumeros {
 
     public static void main(String[] args) {
-        Scanner  numeroUsuario = new Scanner(System.in);
+        Scanner numeroUsuario = new Scanner(System.in);
         int numero;
-        System.out.println("Ingrese un numero");
-        numero=numeroUsuario.nextInt();
 
-        while (numero>0 && numero <9){
-            System.out.println("una cifra");
-        }
+        do {
+            System.out.println("Ingrese un número (0 para salir):");
+            numero = numeroUsuario.nextInt();
+
+            if (numero > 0 && numero < 10) {
+                System.out.println("una cifra");
+            }
+
+        } while (numero != 0);
+
+        System.out.println("Programa terminado");
     }
 }
